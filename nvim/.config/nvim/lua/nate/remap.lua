1f4c5fc13256d6dbd53a75c89d7b72eb5604c5bb
Natehhggh <natehhggh@gmail.com>
2026-04-21 23:30:51 -0700

    pushing for laptop

--- nvim/.config/nvim/lua/nate/remap.lua
+++ nvim/.config/nvim/lua/nate/remap.lua
@@ -4,9 +4,9 @@ vim.keymap.set("n", "<leader>fv", vim.cmd.Ex)
 
 
 --Moves highlighted text with cursor and indents
+
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
-
 
 -- keeps cursor home on J command
 vim.keymap.set("n", "J", "mzJ`z")
@@ -60,5 +60,20 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 -- rename hovered word
 vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><left>")
 
+-- build and run
+vim.keymap.set("n", "<F5>", ":term ./build.sh && ./game")
+
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
 
 
+
+--vim.keymap.set("t", "<C-h>", "<cmd>wincmd h<CR>")
+--vim.keymap.set("t", "<C-j>", "<cmd>wincmd j<CR>")
+--vim.keymap.set("t", "<C-k>", "<cmd>wincmd k<CR>")
+--vim.keymap.set("t", "<C-l>", "<cmd>wincmd l<CR>")
+
+vim.keymap.set("n", "<TAB>", ":bn<CR>")
+vim.keymap.set("n", "<S-TAB>", ":bp<CR>")
